d23313fd77029e227368dbb4a0365133380e5918
Majd Maghamez <majdmaghamez@gmail.com>
2022-10-29 15:46:20 -0400

    added dynamic directories and updated the unit tests

--- UnitTesting/FileMover.cs
+++ UnitTesting/FileMover.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,14 +14,18 @@ namespace UnitTesting
         public void FileMover_hasWriteAccess_CreatedFileExists()
         {
             string source = ConfigurationManager.AppSettings["source"];
+            if (!Directory.Exists(source)) Directory.CreateDirectory(source);
 
             string completeURL = Path.Combine(source, _testFile);
 
             if (File.Exists(completeURL))
                 File.Delete(completeURL);
 
-
-            File.Create(completeURL);
+            using (FileStream fs = File.Create(completeURL))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                fs.Write(info, 0, info.Length);
+            }
 
             Assert.IsTrue(File.Exists(completeURL));
         }
@@ -29,13 +34,18 @@ namespace UnitTesting
         public void FileMover_movingFile_MovedFileExists()
         {
             string source = ConfigurationManager.AppSettings["source"];
+            if (!Directory.Exists(source)) Directory.CreateDirectory(source);
+
             string target = ConfigurationManager.AppSettings["target"];
+            if (!Directory.Exists(target)) Directory.CreateDirectory(target);
 
             string completeSourceURL = Path.Combine(source, _testFile);
             string completeTargetURL = Path.Combine(target, _testFile);
 
-            if (File.Exists(completeTargetURL))           
-                File.Move(completeSourceURL, completeTargetURL);
+            if (File.Exists(completeTargetURL))
+                File.Delete(completeTargetURL);
+
+            File.Move(completeSourceURL, completeTargetURL);
 
             Assert.IsTrue(File.Exists(completeTargetURL));
         }
